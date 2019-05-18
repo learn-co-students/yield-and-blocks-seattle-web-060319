e8d4f95f2b2863a0ba5fcd7d33e275f96ae106c7
Dex Fitch <fitch.dexter@gmail.com>
2019-05-17 18:53:53 -0700

    Yielding

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+    i = 0
 
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!\n"
+  end
 end
 
-# call your method here!
+a = ["Tom", "Tina", "Guiseppe"]
 
+hello_t(a) do |name|
+  if name.start_with?("T")
+    puts "Hi #{name}"
+  end
+end
